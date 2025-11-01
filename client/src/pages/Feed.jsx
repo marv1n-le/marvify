ad8ffe194ffe7dff4897fcb204c265e0aa4383b9
Le Duc Anh <152796553+marv1n-le@users.noreply.github.com>
2025-11-01 22:46:27 +0700

    feat: recent messages ui

--- client/src/pages/Feed.jsx
+++ client/src/pages/Feed.jsx
@@ -1,8 +1,9 @@
-import React, { useEffect, useState } from 'react'
-import { assets, dummyPostsData } from '../assets/assets';
-import Loading from '../components/Loading';
-import StoriesBar from '../components/StoriesBar';
-import PostCard from '../components/PostCard';
+import React, { useEffect, useState } from "react";
+import { assets, dummyPostsData } from "../assets/assets";
+import Loading from "../components/Loading";
+import StoriesBar from "../components/StoriesBar";
+import PostCard from "../components/PostCard";
+import RecentMessages from "../components/RecentMessages";
 
 const Feed = () => {
   const [feeds, setFeeds] = useState([]);
@@ -10,21 +11,21 @@ const Feed = () => {
 
   const fetchFeeds = async () => {
     setFeeds(dummyPostsData);
-  }
+  };
 
   useEffect(() => {
     fetchFeeds();
     setLoading(false);
   }, []);
 
-  return !loading ?(
+  return !loading ? (
     <div className="h-full overflow-y-scroll no-scrollbar py-10 xl:pr-5 flex items-start justify-center xl:gap-8">
       {/* Stories and post list */}
       <div>
         <StoriesBar />
         <div className="p-4 space-y-6">
           {feeds.map((post) => (
-            <PostCard key={post._id} post={post}/>
+            <PostCard key={post._id} post={post} />
           ))}
         </div>
       </div>
@@ -33,14 +34,23 @@ const Feed = () => {
       <div className="max-xl:hidden sticky top-0">
         <div className="max-w-xs bg-white text-xs p-4 rounded-md inline-flex flex-col gap-2 shadow">
           <h3 className="text-slate-800 font-semibold">Sponsored</h3>
-          <img src={assets.sponsored_img} alt="" className="w-75 h-50 rounded-md" />
+          <img
+            src={assets.sponsored_img}
+            alt=""
+            className="w-75 h-50 rounded-md"
+          />
           <p className="text-slate-600">Email marketing</p>
-          <p className="text-slate-400">Get the best tips for your email campaigns. Contact ducanhle.iter@gmail.com for more information.</p>
+          <p className="text-slate-400">
+            Get the best tips for your email campaigns. Contact
+            ducanhle.iter@gmail.com for more information.
+          </p>
         </div>
-        <h1>Recent messages</h1>
+        <RecentMessages />
       </div>
     </div>
-  ) : <Loading />;
-}
+  ) : (
+    <Loading />
+  );
+};
 
-export default Feed
+export default Feed;
