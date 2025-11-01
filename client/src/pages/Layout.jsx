53eacfca1d1c05bc29f2cdaaf324f8b46c2e43c2
Le Duc Anh <152796553+marv1n-le@users.noreply.github.com>
2025-11-01 10:05:49 +0700

    clerk auth setup

--- client/src/pages/Layout.jsx
+++ client/src/pages/Layout.jsx
@@ -1,11 +1,33 @@
-import React from 'react'
+import React, { useState } from "react";
+import Sidebar from "../components/Sidebar";
+import { Menu, X } from "lucide-react";
+import { dummyUserData } from "../assets/assets";
 
 const Layout = () => {
-  return (
-    <div>
-      
+  const user = dummyUserData
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+  return user ? (
+    <div className="w-full flex h-screen">
+      <Sidebar />
+      <div className="flex-1 bg-slate-50">
+        {/* Outlet for nested routes */}
+        <Outlet />
+      </div>
+      {sidebarOpen ? (
+        <X
+          className="absolute top-3 right-3 p-2 z-100 bg-white rounded-md shadow w-10 h-10 text-gray-600 sm:hidden"
+          onClick={() => setSidebarOpen(false)}
+        />
+      ) : (
+        <Menu
+          className="absolute top-3 right-3 p-2 z-100 bg-white rounded-md shadow w-10 h-10 text-gray-600 sm:hidden"
+          onClick={() => setSidebarOpen(true)}
+        />
+      )}
     </div>
-  )
-}
+  ) : (
+    <h1>Loading...</h1>
+  );
+};
 
-export default Layout
+export default Layout;
