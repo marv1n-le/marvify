10b320910ec78f4708f87fcd409b7ad5a73f9be2
Le Duc Anh <152796553+marv1n-le@users.noreply.github.com>
2025-11-03 17:04:57 +0700

    config backend system

---
+++ server/configs/db.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+  try {
+    mongoose.connection.on("connected", () => {
+      console.log("Mongoose connected to DB");
+    });
+    await mongoose.connect(`${process.env.MONGODB_URL}/marvify`);
+  } catch (error) {
+    console.error("MongoDB connection failed:", error);
+  }
+}
+
+export default connectDB;
