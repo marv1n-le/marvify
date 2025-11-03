1e666d1c54273118a84eca9f5fbe50ff4f68ff97
Le Duc Anh <152796553+marv1n-le@users.noreply.github.com>
2025-11-03 21:51:11 +0700

    fix: create user via clerk

--- client/src/pages/ChatBox.jsx
+++ client/src/pages/ChatBox.jsx
@@ -18,7 +18,7 @@ const ChatBox = () => {
   return (
     user && (
       <div className="flex flex-col h-screen">
-        <div className="flex items-center gap-2 p-2 md:px-10 xl:pl-42 bg-gradient-to-r from-indigo-50 to-purple-50 border-b border-gray-300">
+        <div className="flex items-center gap-2 p-2 md:px-10 xl:pl-42 bg-linear-to-r from-indigo-50 to-purple-50 border-b border-gray-300">
           <img
             src={user.profile_picture}
             className="size-8 rounded-full"
