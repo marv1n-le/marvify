e2ae944f01f8e1ad7b48a3df8c25937e3c07e03b
Le Duc Anh <152796553+marv1n-le@users.noreply.github.com>
2025-11-06 03:06:05 +0700

    feat: create and get stories

---
+++ server/models/Story.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+const storySchema = new mongoose.Schema(
+  {
+    user: {
+      type: String,
+      ref: "User",
+      required: true,
+    },
+    content: {
+      type: String,
+    },
+    media_url: {
+      type: String,
+    },
+    media_type: {
+      type: String,
+      enum: ["image", "video", "text"],
+    },
+    views_count: [{
+      type: String,
+      ref: "User",
+    }],
+    background_color: {
+      type: String,
+    },
+  },
+  { timestamps: true, minimize: false }
+);
+
+const Story = mongoose.model("Story", storySchema);
+
+export default Story;
